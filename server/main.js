d322c3eb457a54193cea1e84a7e24fb822fab66e
Amami Ruri <babichan@outlook.com>
2021-09-26 18:49:54 +0800

    使用node代理

---
+++ server/main.js
@@ -0,0 +1,17 @@
+const Fastify = require('fastify')
+const fastify = Fastify({
+    logger: true, keepAliveTimeout: 60 * 1000
+})
+
+fastify.register(require('fastify-cors'), {
+    methods: ['GET', 'POST', 'OPTIONS'],
+    credentials: true,
+    origin: true,
+    maxAge: 60 * 60 * 4
+})
+
+fastify.register(require('fastify-http-proxy'), {
+    upstream: 'http://api.waditu.com'
+})
+
+fastify.listen(4086, err => { if (err) throw err })
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -134,7 +134,7 @@ export class AppComponent implements OnInit {
 
   requestTushare(param: any): Observable<AjaxResponse> {
     return ajax.post(
-      'http://localhost:4200/api',
+      'https://localhost:4086',
       Object.assign({ token: this.async.transform(this.token) }, param),
       { 'Content-Type': 'application/json' }
     );
